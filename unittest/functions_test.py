91b7929754309477db94ceba6d8c1e1d1dad7bf3
lsh110600 <lsh110600@naver.com>
2021-05-15 15:33:51 +0900

    using fine test case

--- unittest/functions_test.py
+++ unittest/functions_test.py
@@ -11,37 +11,38 @@ from functions import add, sub, div, mul
 
 def test_add():
     assert add(1,3) == 4 
-    assert add(1, '2') == "Query Parameter's Data Type is not a String."
-    assert add('1', 2) == "Query Parameter's Data Type is not a String."
-    assert add('a', '2') == "Num1 is not a Number."
-    assert add('2', 'b') == "Num2 is not a Number."
-    assert add('a', 'b') == "Num1 is not a Number."
+    # assert add(1, '2') == "Query Parameter's Data Type is not a String."
+    # assert add('1', 2) == "Query Parameter's Data Type is not a String."
+    # assert add('a', '2') == "Num1 is not a Number."
+    # assert add('2', 'b') == "Num2 is not a Number."
+    # assert add('a', 'b') == "Num1 is not a Number."
 
 
 def test_sub():
     assert sub(6, 4) == 2
     assert sub(4, 6) == -2
-    assert sub(1, '2') == "Query Parameter's Data Type is not a String."
-    assert sub('1', 2) == "Query Parameter's Data Type is not a String."
-    assert sub('a', '2') == "Num1 is not a Number."
-    assert sub('2', 'b') == "Num2 is not a Number."
-    assert sub('a', 'b') == "Num1 is not a Number."
+    # assert sub(1, '2') == "Query Parameter's Data Type is not a String."
+    # assert sub('1', 2) == "Query Parameter's Data Type is not a String."
+    # assert sub('a', '2') == "Num1 is not a Number."
+    # assert sub('2', 'b') == "Num2 is not a Number."
+    # assert sub('a', 'b') == "Num1 is not a Number."
 
 def test_mul():
     assert mul(2, 4) == 8
-    assert mul(1, '2') == "Query Parameter's Data Type is not a String."
-    assert mul('1', 2) == "Query Parameter's Data Type is not a String."
-    assert mul('a', '2') == "Num1 is not a Number."
-    assert mul('2', 'b') == "Num2 is not a Number."
-    assert mul('a', 'b') == "Num1 is not a Number."
+    # assert mul(1, '2') == "Query Parameter's Data Type is not a String."
+    # assert mul('1', 2) == "Query Parameter's Data Type is not a String."
+    # assert mul('a', '2') == "Num1 is not a Number."
+    # assert mul('2', 'b') == "Num2 is not a Number."
+    # assert mul('a', 'b') == "Num1 is not a Number."
 
 def test_div():
-    assert div(1, '2') == "Query Parameter's Data Type is not a String."
-    assert div('1', 2) == "Query Parameter's Data Type is not a String."
-    assert div('a', '2') == "Num1 is not a Number."
-    assert div('2', 'b') == "Num2 is not a Number."
-    assert div('a', 'b') == "Num1 is not a Number."
-    assert div('2', '0') == 'division by zero'
+    assert mul(4, 2) == 2
+    # assert div(1, '2') == "Query Parameter's Data Type is not a String."
+    # assert div('1', 2) == "Query Parameter's Data Type is not a String."
+    # assert div('a', '2') == "Num1 is not a Number."
+    # assert div('2', 'b') == "Num2 is not a Number."
+    # assert div('a', 'b') == "Num1 is not a Number."
+    # assert div('2', '0') == 'division by zero'
 
 
 
